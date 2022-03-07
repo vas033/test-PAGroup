5b8bda1b1b1af6f5df345c74097d0011dd4027a9
Vasilii <ahmadiyarov1992@gmail.com>
2022-03-07 23:02:19 +0200

    added animation show/hide to header

--- src/App/Header/Header.tsx
+++ src/App/Header/Header.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './header.css';
 import { Logo } from './Logo';
 import { NavHeader } from './NavHeader';
 
 export function Header() {
+  const useScrollPosition = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+  
+    useEffect(() => {
+      const updatePosition = () => {
+        setScrollPosition(window.pageYOffset);
+      }
+      window.addEventListener("scroll", updatePosition);
+      updatePosition();
+      return () => window.removeEventListener("scroll", updatePosition);
+    }, []);
+  
+    return scrollPosition;
+  };
+
+  let scrollPosition = useScrollPosition();
+  let header;
+  if (scrollPosition > 20) {
+    header =  styles.header + ' ' + styles.hide
+  } else {
+    header = styles.header
+  }
+
+  console.log(scrollPosition)
+
   return (
-    <div className={styles.header}>
+    <div className={header}>
       <Logo />
       <NavHeader />
     </div>
