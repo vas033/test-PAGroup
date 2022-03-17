9e40f87d026ddc141c68cfb4d2e97cc30d083129
Vasilii <ahmadiyarov1992@gmail.com>
2022-03-18 00:09:38 +0200

    animation on footer, fixer first carusel

--- src/App/Body/SliderOne/SliderOne.tsx
+++ src/App/Body/SliderOne/SliderOne.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import styles from './sliderone.css';
-import Slider from "react-slick";
+import carousel from './sliderstyle.css';
 import partnerList from '../../arrays/partnerList';
 import {Image} from '../../../modules/Image/Image';
 
-export default class AutoPlay extends React.Component {
-  render() {
-    const settings = {
-      arrows: false,
-      centerMode: true,
-      infinite: true,
-      autoplay: true,
-      speed: 600,
-      autoplaySpeed: 600,
-      cssEase: 'linear',
-      variableWidth: true,
-    };
-    return (
-        <Slider className={styles.slider} {...settings}>
-          { partnerList.map( (item) => <Image cl={styles.image} src={item.src} alt={item.name} key={item.name}/>) }
-        </Slider>
-    );
-  }
+export default function SliderOneTest() {
+  return (
+    <div className={carousel.maindiv}>
+      <div className={carousel.div1}>
+        {partnerList.map((item) => <Image cl={styles.image} src={item.src} alt={item.name} key={item.name} />)}
+      </div>
+      <div className={carousel.div2}>
+        {partnerList.map((item) => <Image cl={styles.image} src={item.src} alt={item.name} key={item.name} />)}
+      </div>
+    </div>
+  )
 }
--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from '../../modules/className';
+import getCoords from '../../modules/getCoords';
 import navList from '../arrays/navList';
 import styles from './footer.css';
 
 export function Footer() {
+  const [scrollPos, setScrollPosition] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.pageYOffset);
+      setWindowHeight(window.innerHeight);
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+    return () => {
+      window.removeEventListener("scroll", updatePosition);
+    }
+  }, []);
+
+
+  if (windowWidth > 1200) {
+
+    const footer = document.getElementById('footer');
+    const content = document.getElementById('footerContent')
+    let coords;
+
+    if (footer && content) {
+      coords = getCoords(footer);
+      footer.style.overflow = 'hidden';
+      content.style.transform = 'translate3d( 0, -100%, 0)';
+      content.style.willChange = 'transform';
+
+      let currentPos = scrollPos + windowHeight;
+
+      if (currentPos > coords.top) {
+
+        let tenThousandth = (coords.bottom - coords.top) / 10000;
+        let showedBlockHeight = currentPos - coords.top;
+        let percent = -100 + showedBlockHeight * tenThousandth;
+
+        if (content && percent <= 0) {
+          content.style.transform = `translate3d( 0, ${percent}%, 0)`;
+          content.style.willChange = 'transform';
+        } else {
+          content.style.transform = 'translate3d( 0, 0, 0)';
+        }
+      }
+    }
+  }
+
+
   return (
-    <footer className={classNames(styles.container, styles.flexColumn)}>
-      <h2 className={styles.title}>Сделаем уже что-нибудь?</h2>
-      <a className={styles.hiddenLink} href="mailto:dsgn@pagroup.ru">Давайте начнём</a>
-      <div className={styles.infoNav}>
-        <div className={styles.infoContainer}>
-          <a className={styles.mail} href="mailto:dsgn@pagroup.ru">dsgn@pagroup.ru</a>
-          <div className={styles.firmTaxInfo}>
-            <p className={styles.firmInfo}>ИП Антипов ПН</p>
-            <p className={styles.firmInfo}>ИНН 550516521644</p>
+    <footer id='footer'>
+      <div id='footerContent' className={classNames(styles.container, styles.flexColumn)}>
+        <h2 className={styles.title}>Сделаем уже что-нибудь?</h2>
+        <a className={styles.hiddenLink} href="mailto:dsgn@pagroup.ru">Давайте начнём</a>
+        <div className={styles.infoNav}>
+          <div className={styles.infoContainer}>
+            <a className={styles.mail} href="mailto:dsgn@pagroup.ru">dsgn@pagroup.ru</a>
+            <div className={styles.firmTaxInfo}>
+              <p className={styles.firmInfo}>ИП Антипов ПН</p>
+              <p className={styles.firmInfo}>ИНН 550516521644</p>
+            </div>
           </div>
+          <nav className={classNames(styles.flexColumn, styles.nav)}>
+            {navList.map(el =>
+              <a className={styles.navLinks} key={navList.indexOf(el)} href={el.link}>{el.name}</a>
+            )}
+          </nav>
         </div>
-        <nav className={classNames(styles.flexColumn, styles.nav)}>
-          {navList.map(el =>
-            <a className={styles.navLinks} key={navList.indexOf(el)} href={el.link}>{el.name}</a>
-          )}
-        </nav>
+        <p className={styles.lisence}>Любое использование материалов сайта без разрешения запрещено</p>
       </div>
-      <p className={styles.lisence}>Любое использование материалов сайта без разрешения запрещено</p>
     </footer>
   );
 }
